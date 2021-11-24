57bfc00ba1d8c20b74543d01fcefe5449698370f
teredet <artem.vanzytler@gmail.com>
2021-11-24 14:50:06 +0200

    Fix some mistake.

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ from .views import home, posts, post, sendEmail
 
 urlpatterns = [
     path('', home, name="home"),
-    path('posts/', posts, name="posts"),
+    path('projects/', posts, name="posts"),
     path('post/<slug:post_slug>/', post, name="post"),
     path('send_email/', sendEmail, name="send_email"),
 ]
